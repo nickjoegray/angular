3b12550877f7de5a67eb26d4844b57f688cd483d
Nick Gray <nickjoegray@gmail.com>
2014-03-01 13:11:57 -0500

    add images to each product

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,7 +6,7 @@ var vantageApp = angular.module('vantageApp', []);
 
 vantageApp.controller('ProductsCtrl', function($scope, $http) {
   $http.get('json/products.json').success(function(data) {
-    $scope.products = data.splice(0,19);
+    $scope.products = data.splice(0,20);
   });
 
   $scope.orderProp = 'name';
