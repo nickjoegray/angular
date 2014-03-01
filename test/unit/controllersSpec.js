bad505a1b88b85205fe64fe72cc2933a07253fa9
Nick Gray <nickjoegray@gmail.com>
2014-03-01 11:12:17 -0500

    add test for

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -5,14 +5,25 @@
 describe('vantageApp controllers', function() {
   
   describe('ProductsCtrl', function(){
-    var scope, ctrl;
+    var scope, ctrl, $httpBackend;
   
     beforeEach(module('vantageApp'));
   
-    beforeEach(inject(function($controller) {
-      scope = {};
-      ctrl = $controller('ProductsCtrl', {$scope:scope});
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('json/products.json').
+          respond([{name: 'Coors Light'}]);
+
+      scope = $rootScope.$new();
+          ctrl = $controller('ProductsCtrl', {$scope: scope});
     }));
+
+    it('should create "products" model with 1 product fetched from xhr', function() {
+      expect(scope.products).toBeUndefined();
+      $httpBackend.flush();
+
+      expect(scope.products).toEqual([{name: 'Coors Light'}]);
+    });
  
     it('should set the default value of orderProp model', function() {
       expect(scope.orderProp).toBe('result[0].name');
