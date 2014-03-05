62b2001ba62abd4382a5c7181790878d24a30ba2
Nick Gray <nickjoegray@gmail.com>
2014-03-04 21:46:19 -0500

    capture JSONP LCBO data via  callback

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,9 +5,22 @@
 var vantageApp = angular.module('vantageApp', []);
 
 vantageApp.controller('ProductsCtrl', function($scope, $http) {
-  $http.get('json/products.json').success(function(data) {
-    $scope.products = data.splice(0,20);
-  });
+  // $http({method: 'GET', url: 'http://lcboapi.com/products'}).
+  //   success(function(data, status, headers, config) {
+  //     $scope.products = data;
+  // });
 
   $scope.orderProp = 'name';
+
+  var url = "http://lcboapi.com/products?callback=JSON_CALLBACK";
+
+  $http.jsonp(url)
+    .success(function(data){
+        console.log(data);
+        $scope.products = data.result;
+    });
+
+  for (var i = 0; i < $scope.products.length; i++) {
+    console.log(i);
+  }
 });
